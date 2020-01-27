ce7224a240c678dce65766e6c0a13fd31246a0e1
Oleksii Kosobutskyi <oleksii.kosobutskyi.stud@pw.edu.pl>
2020-01-27 04:16:06 +0100

    First push to remote

---
+++ Assets/BlockSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSystem : MonoBehaviour {
+
+    [SerializeField]
+    private BlockType[] allBlockTypes;
+
+    [HideInInspector]
+    public Dictionary<int, Block> allBlocks = new Dictionary<int, Block>();
+
+    private void Awake() {
+        for (int i = 0; i < allBlockTypes.Length; i++) {
+            BlockType newBlockType = allBlockTypes[i];
+            Block newBlock = new Block(i, newBlockType.blockName, newBlockType.blockMaterial);
+            allBlocks[i] = newBlock;
+            Debug.Log("Block added to dictionary " + allBlocks[i].blockName);
+        }
+    }
+}
+
+public class Block {
+    public int blockID;
+    public string blockName;
+    public Material blockMaterial;
+
+    public Block (int id, string name, Material material) {
+        blockID = id;
+        blockName = name;
+        blockMaterial = material;
+    }
+}
+
+[Serializable]
+public struct BlockType {
+    public string blockName;
+    public Material blockMaterial;
+}
